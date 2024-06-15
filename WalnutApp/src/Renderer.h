d65f95dbb26db20f5a155d037b873f90e161c43e
Michal Šesták <lertcze@gmail.com>
2024-06-15 10:25:05 +0200

    Multiple Spheres and "Ray Tracing Pipeline"

--- WalnutApp/src/Renderer.h
+++ WalnutApp/src/Renderer.h
@@ -4,6 +4,7 @@
 
 #include "Camera.h"
 #include "Ray.h"
+#include "Scene.h"
 
 #include <memory>
 #include <glm/glm.hpp>
@@ -14,12 +15,28 @@ public:
 	Renderer() = default;
 
 	void OnResize(uint32_t width, uint32_t height);
-	void Render(const Camera& camera);
+	void Render(const Scene& scene, const Camera& camera);
 
 	std::shared_ptr<Walnut::Image> GetFinalImage() const { return m_FinalImage; }
 private:
-	glm::vec4 TraceRay(const Ray& ray);
+	struct HitPayload
+	{
+		float HitDistance;
+		glm::vec3 WorldPosition;
+		glm::vec3 WorldNormal;
+
+		int ObjectIndex;
+	};
+	glm::vec4 PerPixel(uint32_t x, uint32_t y); // RayGen
+
+	HitPayload TraceRay(const Ray& ray);
+	HitPayload ClosestHit(const Ray& ray, float hitDistance, int objectIndex);
+	HitPayload Miss(const Ray& ray);
 private:
 	std::shared_ptr<Walnut::Image> m_FinalImage;
+
+	const Scene* m_ActiveScene = nullptr;
+	const Camera* m_ActiveCamera  = nullptr;
+
 	uint32_t* m_ImageData = nullptr;
 };
