0601acec6e8df802a2c4979bca392cd70e3c8c96
Michal Šesták <lertcze@gmail.com>
2024-06-16 14:47:34 +0200

    thread_local keyword for random gen

--- Walnut/src/Walnut/Random.cpp
+++ Walnut/src/Walnut/Random.cpp
@@ -2,7 +2,7 @@
 
 namespace Walnut {
 
-	std::mt19937 Random::s_RandomEngine;
+	thread_local std::mt19937 Random::s_RandomEngine; // thread_local create an instance per core
 	std::uniform_int_distribution<std::mt19937::result_type> Random::s_Distribution;
 
 }
